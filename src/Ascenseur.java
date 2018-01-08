80769cc4fcb2b9e810c2350895e85e93b978fef5
matthieujan <matthieujan@protonmail.com>
2018-01-08 12:26:42 +0100

    Init

--- src/Ascenseur.java
+++ src/Ascenseur.java
@@ -92,16 +92,43 @@ public class Ascenseur extends Thread
      */    
     private Direction choisirDirection ()
     {
+    	// 3. Choose direction
     	Direction ret =Direction.NONE;
-
-    	
-    	if (DEBUG)
-    		System.out.println(
-    				" >> Je suis un printout de deboguage - Utilisez-moi!!\n" +
-    				" >> La methode choisirDirection() n'est pas implantee!");
-    	
-    	// [... a completer selon les specifications ...]
-    		   	
+    	// 3.1 if AscDestUp or AscDestNone
+    	if(dir.equals(Direction.UP) || dir.equals(Direction.NONE)){
+    	    // 3.1.1 If call up or FloorDestUp
+    		if(appelAuDessus(etage) || destAuDessus(etage)){
+    			//Stay with AscDirUP
+				ret = Direction.UP;
+			}
+			// 3.1.2 If call down or FloorDestDown
+			else if(appelEnDessous(etage) || destEnDessous(etage)){
+    			//Set AscDirDown
+				ret = Direction.DOWN;
+			}
+			// 3.1.3 Else, stay the same
+			else{
+    			// Stay at AscDirNone
+				ret = Direction.NONE;
+			}
+		}
+		// 3.2 If AscDirDown
+		else if(dir.equals(Direction.DOWN)){
+			// 3.2.1 if call down or FloorDestDown
+			if(appelEnDessous(etage) || destEnDessous(etage)){
+				//Keep down
+				ret = Direction.DOWN;
+			}
+			// 3.2.2 if call up or FloorDestUp
+            else if(appelAuDessus(etage) || destAuDessus(etage)){
+    			//Set AscDirUp
+				ret = Direction.UP;
+			}
+			// 3.2.3 Else, set AscDirNone
+			else {
+    			ret = Direction.NONE;
+			}
+		}
     	return ret;
     }
     
@@ -124,6 +151,30 @@ public class Ascenseur extends Thread
     	for (int i=0; i<etageCourant-1; i++)
     	{
     		if (sim.appels[i]!=Direction.NONE)
+    			ret =true;
+    	}
+
+    	return ret;
+    }
+
+    private boolean destAuDessus(int etageCourant)
+    {
+    	boolean ret =false;
+    	for (int i=Constantes.ETAGES-1; i>=etageCourant; i--)
+    	{
+    		if (sim.destinations[i])
+    			ret =true;
+    	}
+
+    	return ret;
+    }
+
+    private boolean destEnDessous(int etageCourant)
+    {
+    	boolean ret =false;
+    	for (int i=0; i<etageCourant-1; i++)
+    	{
+    		if (sim.destinations[i])
     			ret =true;
     	}
 
